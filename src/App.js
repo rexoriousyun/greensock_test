380b6b7e0afc0f36d0496c994b78021479ad57d7
rexoriousyun <chgchg123@naver.com>
2018-01-16 13:27:59 -0500

    setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
